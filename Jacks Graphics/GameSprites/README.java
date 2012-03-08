f1a98cdcf2e49571e89154749ec958087645251b
unknown <Infected@Malware-Trojan.(none)>
2012-03-08 14:09:38 -0600

    finished all the hero portraits monster avatars for first level and sprites

---
+++ Jacks Graphics/GameSprites/README.java
@@ -0,0 +1,44 @@
+--------
+|README|
+--------
+
+*****THIS DOCUMENT INCLUDES 3 FOLDERS*********
+
+
+------TABLE OF CONTENTS------
+	
+	1. HERO
+	2. MONSTERS
+	3. SPRITES
+
+-----------------------------
+
+
+
+===============
+\\\\HERO\\\\\\
+===============
+I made 8 portraits, all of which are 200x200
+Four of these are male, four of these are female
+
+They are labeled with H_##, H represents "Hero".
+I have not decided on what element they should 
+be yet so that's up for voting.
+
+===============
+\\\\MONSTERS\\\
+===============
+
+Every file is encoded in the format of M_#_#
+M means Monster, the first number represents the 
+level of which the monster will appear and the 
+second number is the order number given to the monsters
+
+===============
+\\\\SPRITES\\\\
+===============
+
+This is a collection of sprites I have cut out, I'm not
+sure where we could use these but since I put some work
+into it I thought it'd be a waste to just trash it. maybe
+we'll find a use later?
